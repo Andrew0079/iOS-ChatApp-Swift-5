8bef88b6bef947beae9b48b385cc84e86d570762
Andras Strublics <andrasstrublics@Andrass-MacBook-Pro.local>
2020-08-16 15:29:21 +0100

    Getting Started

---
+++ Messenger/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Messenger
+//
+//  Created by Andras Strublics on 16/08/2020.
+//  Copyright © 2020 Andras Strublics. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
